e2213af41d986fd5abbeb30920d1936ded6fc258
Ryan Hunt <ryanjosephhunt@gmail.com>
2015-11-05 22:16:07 -0500

    Forgot to download the data file

--- loadData.R
+++ loadData.R
@@ -2,7 +2,8 @@ library(dplyr)
 library(lubridate)
 
 readData <- function () {
-tbl_df(read.table("household_power_consumption.txt",stringsAsFactors = FALSE,sep=";",header = TRUE,na.strings = "?")) %>% 
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="exdata_data_household_power_consumption.zip")
+tbl_df(read.table(unz("exdata_data_household_power_consumption.zip","household_power_consumption.txt"),stringsAsFactors = FALSE,sep=";",header = TRUE,na.strings = "?")) %>% 
      mutate(
                TypedDate = as.Date(strptime(Date,"%d/%m/%Y")),
                DateTime  = dmy_hms(paste(Date, Time)),
